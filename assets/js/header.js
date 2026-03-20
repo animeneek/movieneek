3a72226ac57f15bb3b45dc84379640da9d2e6470
animeneek <imemyanime@gmail.com>
2026-03-20 20:14:49 +0400

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,8 @@
-
+// header.js
+async function loadHeader() {
+    const headerContainer = document.getElementById('header-container');
+    const res = await fetch('header.html');
+    const html = await res.text();
+    headerContainer.innerHTML = html;
+}
+loadHeader();
